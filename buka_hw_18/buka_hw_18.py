07082cc634ae983630cd3a719c2ac4b84ed8b425
number3208 <letov2110@gmail.com>
2024-01-26 17:03:09 +0300

    hw

---
+++ buka_hw_18/buka_hw_18.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+q=0
+
+@app.route('/',methods=['GET'])
+def hello():
+    global q
+    return f'{q}'
+@app.route('/plus',methods=['POST'])
+def plus():
+    global q
+    q+=1
+    return f'{q}'
+@app.route('/minus',methods=['POST'])
+def minus():
+    global q
+    q-=1
+    return f'{q}'
+if __name__ =='__main__':
+    app.run()
